a493a1eab4b6ce03f0eb7d7dfea9d819680aca18
nmkdev <nguyenminhkhoi.nmk.mail@gmail.com>
2024-10-25 19:10:41 +0700

    Update

--- organizations/ntmk/problems/ntmk_triangular/ntmk_triangular.cpp
+++ organizations/ntmk/problems/ntmk_triangular/ntmk_triangular.cpp
@@ -1,8 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const long long MAXARR = 1e7+1;
+long long n, sotamgiac=0, arr[MAXARR], khoitao = 1;
 
+bool check(int number){
+    if (sotamgiac > number)
+        return 0;
+    while (sotamgiac < number){
+        if (number == sotamgiac)
+            return 1;
+        sotamgiac+=khoitao;
+        khoitao++;
+    }
+    if (sotamgiac == number)
+        return 1;
+    return 0;
+}
+
+void readdata(){
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+}
+
+
+void solve(){
+    for (int i = 0; i < n; i++){
+        if (check(arr[i]))
+            cout << 1 << '\n';
+        else
+            cout << 0 << '\n';
+    }
+}
 
 int main(){
-
+    readdata();
+    solve();
+    return 0;
 }
